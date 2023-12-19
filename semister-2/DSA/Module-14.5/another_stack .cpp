591ad98c3dba11d9019ad8fba04fabbeee2e622f
Tanvir <tntanvir2382018@gmail.com>
2023-12-19 18:46:44 +0600

    solved another problem

---
+++ semister-2/DSA/Module-14.5/another_stack .cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int vlu;
+    Node *next;
+    Node *prev;
+    Node(int vlu)
+    {
+        this->vlu = vlu;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class myStack
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+    void push(int n)
+    {
+        sz++;
+        Node *newNode = new Node(n);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+    void pop()
+    {
+        sz--;
+        Node *delNode = tail;
+        tail = tail->prev;
+        if (tail == NULL)
+        {
+            head = NULL;
+        }
+        else
+        {
+            tail->next = NULL;
+        }
+        delete delNode;
+    }
+    int top()
+    {
+        return tail->vlu;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        if (sz == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+int main()
+{
+    myStack s;
+    myStack n;
+    int m;
+    cin >> m;
+    while (m--)
+    {
+        int x;
+        cin >> x;
+        s.push(x);
+    }
+    while (!s.empty())
+    {
+        // cout << s.top() << " ";
+        n.push(s.top());
+        s.pop();
+    }
+    while (!n.empty())
+    {
+        cout << n.top() << " ";
+        n.pop();
+    }
+
+    return 0;
+}
