955a82d8658e376637443473daddb57ce44b524d
Tanvir <tntanvir2382018@gmail.com>
2024-06-09 03:31:04 +0600

    done

--- semister-3/Django/week-5/Job&assignment/Mid exam/CarShop/author/urls.py
+++ semister-3/Django/week-5/Job&assignment/Mid exam/CarShop/author/urls.py
@@ -2,10 +2,10 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('singup/',views.singup,name='singup'),
+    path('singup/',views.singup.as_view(),name='singup'),
     path('singin/',views.singin,name='singin'),
     path('singout/',views.logoutUser,name='singout'),
     path('profile/',views.profile,name='profile'),
-    path('profile/edit/',views.profileEdit,name='profileEdit'),
+    path('profile/edit/',views.profileEdit.as_view(),name='profileEdit'),
     
 ]
--- semister-3/Django/week-5/Job&assignment/Mid exam/CarShop/carmodel/urls.py
+++ semister-3/Django/week-5/Job&assignment/Mid exam/CarShop/carmodel/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.carBrand,name='carBrand'),
+    path('', views.CarBrand.as_view(),name='carBrand'),
 ]
